11e8111ed9787024e1224bd4bc9cbe9e20667919
Sebastian Quispe <sebastian.quispe.b@utec.edu.pe>
2026-04-22 13:58:34 -0500

    fix: properly dereference HDF5 object references in TVSum .mat
    MATLAB v7.3 .mat files use HDF5 object references for cell arrays.
    Need to use data[ref] to dereference, not data[array].

--- scripts/prepare_tvsum_annotations.py
+++ scripts/prepare_tvsum_annotations.py
@@ -5,20 +5,29 @@ from pathlib import Path
 # TVSum .mat is v7.3 format (HDF5)
 data = h5py.File('data/raw/tvsum/annotations/ydata-tvsum50.mat', 'r')
 
-# The structure is different from scipy .mat
-# videos are referenced via HDF5 references
-video_ids = data['tvsum50']['video']
+# The structure uses HDF5 references
+# Access the tvsum50 group
+tvsum50 = data['tvsum50']
+
+# video IDs are stored as cell array with HDF5 references
+video_ids = tvsum50['video']
+user_anno = tvsum50['user_anno']
+
 features_dir = Path('data/features')
 
 for i in range(len(video_ids)):
-    # Get video ID (it's stored as bytes)
-    vid_ref = video_ids[i]
-    vid_data = data[vid_ref]
-    video_id = ''.join(chr(c) for c in vid_data[:].flatten())
+    # Dereference the HDF5 reference to get the actual dataset
+    vid_ref = video_ids[i][0]  # Get reference from cell array
+    vid_dataset = data[vid_ref]
     
-    # Get scores
-    scores_ref = data['tvsum50']['user_anno'][i]
-    scores_data = data[scores_ref][:]
+    # Read video ID - stored as uint16 characters
+    video_id_chars = vid_dataset[:].flatten()
+    video_id = ''.join(chr(int(c)) for c in video_id_chars)
+    
+    # Get user annotations
+    anno_ref = user_anno[i][0]
+    anno_dataset = data[anno_ref]
+    scores_data = anno_dataset[:]
     
     # Average across users (20 annotators)
     shot_scores = np.mean(scores_data, axis=0)
@@ -28,7 +37,6 @@ for i in range(len(video_ids)):
         with h5py.File(h5_path, 'a') as f:
             n_frames = f['n_frames'][()]
             # TVSum shots are ~2 seconds, at 2 FPS that's ~4 frames per shot
-            # Simple interpolation: repeat each shot score
             frame_scores = np.repeat(shot_scores, 4)[:n_frames]
             frame_scores = frame_scores.astype(np.float32)
             
